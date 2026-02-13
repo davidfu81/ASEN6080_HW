7c9a0c2597d6bfa97a3cad60c841d2492918cefc
davidfu81 <davidfu100@gmail.com>
2026-02-12 19:08:15 -0700

    update residuals plot

--- Post/plot_meas_resid.m
+++ Post/plot_meas_resid.m
@@ -1,33 +1,40 @@
 % Utility function to plot measurement residuals
-function [fig_resid, ax_resid] = plot_meas_resid(tdata, yest, ytrue, title)
-    
+function [fig_resid, ax_resid] = plot_meas_resid(tdata, yresid, title_str, ...
+    components, units, stations)
+    if nargin < 4
+        components = ["Range", "Range-Rate"];
+        units = ["m", "m/s"];
+    end
+    if nargin < 6
+        stations = [1, 2, 3];
+    end
     fig_resid = figure;
     ax_resid = [];
-    set(gcf, "Position", [100 100 750 600])
-    tl = tiledlayout(2,1);
+    set(gcf, "Position", [100 100 750 500])
+    tl = tiledlayout(length(components),1);
     tl.TileSpacing = 'compact';
     tl.Padding = 'loose';
     
-    ylabels = ["Rho Error [m]", "Rho Dot Error [m/s]"];
-    for i = 1:2
+    for i = 1:length(components)
         ax_resid(i) = nexttile;
         hold on; grid on;
         ax = gca;
         ax.XAxis.Exponent = 0;
         ax.YAxis.Exponent = 0;
 
-        for j = 1:size(ytrue, 3)
-            errors = ytrue(i,:,j) - yest(i,:,j);
-            scatter(tdata, errors*1000, 4)
+        for j = 1:size(yresid, 3)
+            scatter(tdata, yresid(i,:,j), 4)
         end
-        ylabel(ylabels(i))
+        ylabel(sprintf("%s [%s]", components(i), units(i)))
+        rms_resid = sqrt(sum(sum(yresid(i,~isnan(yresid(i,:,:))).^2))/length(tdata));
+        title(sprintf("RMS: %.3e %s", rms_resid, units(i) ))
     end
     xlabel("Time [s]")
-    lgd_labels = strings([1, size(ytrue, 3)]);
+    lgd_labels = strings([1, size(yresid, 3)]);
     for i = 1:3
-        lgd_labels(i) = sprintf("Station %d", i);
+        lgd_labels(i) = sprintf("Station %d", stations(i));
     end
     lgd = legend(lgd_labels, 'NumColumns', length(lgd_labels));
     lgd.Layout.Tile = 'north';
-    sgtitle(title);
+    sgtitle(title_str);
 end
